71f135290fa9723d78fdc12aa984c56291fb402f
lari.cavalcante <lari.cavalcante13@gmail.com>
2026-04-01 15:10:46 -0300

    adjusting margin footer

--- script-home.js
+++ script-home.js
@@ -1,34 +1,3 @@
-/*
-
-function onScanSuccess(decodedText) {
-    // Para o scanner e redireciona
-    html5QrcodeScanner.clear().then(() => {
-        window.location.href = `index.html?id=${decodedText}`;
-    }).catch(error => {
-        console.error("Erro ao limpar scanner", error);
-        window.location.href = `index.html?id=${decodedText}`;
-    });
-}
-
-// Configuração otimizada
-const config = { 
-    fps: 15, // Aumentei um pouco a fluidez
-    qrbox: { width: 250, height: 250 },
-    aspectRatio: 1.0,
-    rememberLastUsedCamera: true // Ajuda o celular a lembrar da câmera traseira
-};
-
-let html5QrcodeScanner = new Html5QrcodeScanner("reader", config, false);
-
-// Renderiza e captura erros de inicialização
-html5QrcodeScanner.render(onScanSuccess, (errorMessage) => {
-    // Erros de leitura comuns (falta de foco) são ignorados
-}, (error) => {
-    // Esse erro aqui é o que importa: se a câmera for negada ou não achada
-    console.error("Erro crítico de inicialização:", error);
-});
-*/
-
 document.addEventListener("DOMContentLoaded", function() {
     
     // 1. Criamos a instância, mas SEM o .render() automático ainda
